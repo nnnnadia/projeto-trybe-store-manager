757d05809f7393410fc44e94a007f13963ed3275
nnnnadia <tristaonadia@gmail.com>
2022-09-22 15:59:00 -0300

    feat: middleware to treat json body.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { productRouter } = require('./routers');
 const { errorHandler } = require('./middlewares');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
