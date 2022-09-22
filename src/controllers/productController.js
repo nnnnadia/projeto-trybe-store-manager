fcc72a523f2d1a8d2c7d95cb2aefd66174871791
nnnnadia <tristaonadia@gmail.com>
2022-09-22 15:57:50 -0300

    feat: create `postProduct` on productController.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -7,6 +7,14 @@ const getProducts = async (req, res, next) => {
   next(result);
 };
 
+const postProduct = async (req, res, next) => {
+  const product = req.body;
+  const result = await productService.registerProduct(product);
+  if (!result.type) return res.status(201).json(result.content);
+  next(result);
+};
+
 module.exports = {
   getProducts,
+  postProduct,
 };
