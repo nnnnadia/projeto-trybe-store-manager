f79cdcdd01f1e531df6fa6841453ee5dbbf2985b
nnnnadia <tristaonadia@gmail.com>
2022-09-26 15:09:41 -0300

    style: index services.

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,9 @@
 const productService = require('./productService');
+const saleProductService = require('./saleProductService');
+const saleService = require('./saleService');
 
 module.exports = {
   productService,
+  saleProductService,
+  saleService,
 };
