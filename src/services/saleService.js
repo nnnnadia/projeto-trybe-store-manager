56c12dc17cbbacbfa2838e1ee676bf1511096fdc
nnnnadia <tristaonadia@gmail.com>
2022-09-26 15:08:30 -0300

    feat: create `registerSale` on saleService.

---
+++ src/services/saleService.js
@@ -0,0 +1,21 @@
+const { saleModel } = require('../models');
+const saleProductService = require('./saleProductService');
+
+const registerSale = async (sales) => {
+  try {
+    const date = new Date();
+    const saleId = await saleModel.createSale(date);
+    await Promise.all(saleProductService.registerSaleProduct(sales, saleId));
+    const sale = {
+      id: saleId,
+      itemsSold: await saleProductService.findSaleProductBySaleId(saleId),
+    };
+    return { type: null, content: sale };
+  } catch (err) {
+    return { type: 'INTERNAL_ERROR', message: err.message };
+  }
+};
+
+module.exports = {
+  registerSale,
+};
