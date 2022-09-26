9e6a6ffee9ad46b01b012a2c4db9aa73f5d39101
nnnnadia <tristaonadia@gmail.com>
2022-09-26 15:37:53 -0300

    test: unit test for productModel.

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -7,20 +7,26 @@ const { productModel } = require('../../../src/models');
 const { productsFromDB, insertIdFromDB } = require('./mocks/productModel.mock');
 
 describe('Teste de unidade do productModel', function () {
-  it('Realizando leitura de dados com o readAllProducts', async function () {
-    sinon.stub(connection, 'execute').resolves(productsFromDB);
-    const result = await productModel.readAllProducts();
-    expect(result).to.be.deep.equals(...productsFromDB);
-  });
-  it('Realizando leitura de apenas um produto', async function () {
-    sinon.stub(connection, 'execute').resolves(productsFromDB);
-    const result = await productModel.readProductById(1);
-    expect(result).to.be.deep.equals(...productsFromDB[0]);
-  });
-  it('Cria um novo produto com o createProduct', async function () {
-    sinon.stub(connection, 'execute').resolves(insertIdFromDB);
-    const result = await productModel.createProduct({ name: 'produto' });
-    expect(result).to.be.equal(42);
-  });
+  describe('.readAllProducts: ', function () {
+    it('Realizando leitura de dados com o readAllProducts', async function () {
+      sinon.stub(connection, 'execute').resolves(productsFromDB);
+      const result = await productModel.readAllProducts();
+      expect(result).to.be.deep.equals(...productsFromDB);
+    });
+});
+  describe('.readProductById: ', function () {
+    it('Realizando leitura de apenas um produto', async function () {
+      sinon.stub(connection, 'execute').resolves(productsFromDB);
+      const result = await productModel.readProductById(1);
+      expect(result).to.be.deep.equals(...productsFromDB[0]);
+    });
+});
+  describe('.createProduct: ', function () {
+    it('Cria um novo produto com o createProduct', async function () {
+      sinon.stub(connection, 'execute').resolves(insertIdFromDB);
+      const result = await productModel.createProduct({ name: 'produto' });
+      expect(result).to.be.equal(42);
+    });
+});
   afterEach(sinon.restore);
 });
