5439a7e02251fbaac4a9da2c4b54d9b20cb69638
nnnnadia <tristaonadia@gmail.com>
2022-09-26 15:16:24 -0300

    style: index middlewares.

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,11 @@
 const errorHandler = require('./errorHandler');
 const productValidation = require('./productValidation');
+const saleValidation = require('./saleValidation');
+const saleProductValidation = require('./saleProductValidation');
 
 module.exports = {
   errorHandler,
   productValidation,
+  saleValidation,
+  saleProductValidation,
 };
