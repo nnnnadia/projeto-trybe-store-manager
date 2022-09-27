6ab0ba6ca8928545144d48e1a9ba6c44f8ea3c4d
nnnnadia <tristaonadia@gmail.com>
2022-09-27 13:51:38 -0300

    feat: get route to `/sales` and `/sales/:id`.

--- src/routers/saleRouter.js
+++ src/routers/saleRouter.js
@@ -4,6 +4,13 @@ const { saleValidation, saleProductValidation } = require('../middlewares');
 
 const router = express.Router();
 
+router.get('/', saleController.getSales);
+router.get(
+  '/:id',
+  saleValidation.idValidation,
+  saleController.getSales,
+);
+
 router.post(
   '/',
   saleValidation.saleValidation,
