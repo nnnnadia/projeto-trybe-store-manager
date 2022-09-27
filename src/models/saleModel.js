14ac47fbdb89ef8bfd710577069ddc1ae672963a
nnnnadia <tristaonadia@gmail.com>
2022-09-27 13:43:15 -0300

    feat: create `readAllSales` and `readSaleProductBySaleIdDated` on saleModel.

--- src/models/saleModel.js
+++ src/models/saleModel.js
@@ -1,5 +1,27 @@
 const connection = require('./connection');
 
+const readAllSales = async () => {
+  const [result] = await connection.execute(
+    `SELECT sale_id AS saleId, date, product_id AS productId, quantity
+    FROM sales_products AS sp
+    JOIN sales AS s
+    ON sp.sale_id = s.id`,
+  );
+  return result;
+};
+
+const readSaleProductBySaleIdDated = async (saleId) => {
+  const [result] = await connection.execute(
+    `SELECT date, product_id AS productId, quantity
+    FROM sales_products AS sp
+    JOIN sales AS s
+    ON sp.sale_id = s.id
+    WHERE s.id = ?`,
+    [saleId],
+  );
+  return result;
+};
+
 const createSale = async (date) => {
   const [{ insertId }] = await connection.execute('INSERT INTO sales (date) VALUE (?)', [date]);
   return insertId;
@@ -7,4 +29,6 @@ const createSale = async (date) => {
 
 module.exports = {
   createSale,
+  readAllSales,
+  readSaleProductBySaleIdDated,
 };
