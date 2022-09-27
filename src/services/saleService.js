5abcce89738b0bee2da2099b59d8002e6e999c52
nnnnadia <tristaonadia@gmail.com>
2022-09-27 13:48:10 -0300

    feat: create `findSales` on saleService.

--- src/services/saleService.js
+++ src/services/saleService.js
@@ -1,6 +1,23 @@
 const { saleModel } = require('../models');
 const saleProductService = require('./saleProductService');
 
+const findSales = async (id) => {
+  try {
+    const sales = {
+      type: null,
+      content: !id
+        ? await saleModel.readAllSales()
+        : await saleModel.readSaleProductBySaleIdDated(id),
+    };
+    if (sales.content.length !== 0) return sales;
+    return {
+      type: 'SALE_NOT_FOUND', message: 'Sale not found',
+    };
+  } catch (err) {
+    return { type: 'INTERNAL_ERROR', message: 'Internal error' };
+  }
+};
+
 const registerSale = async (sales) => {
   try {
     const date = new Date();
@@ -17,5 +34,6 @@ const registerSale = async (sales) => {
 };
 
 module.exports = {
+  findSales,
   registerSale,
 };
