ebb4ccfcf036152c143a89049559e2a046612599
nnnnadia <tristaonadia@gmail.com>
2022-09-29 16:10:18 -0300

    feat: create `changeSale` on saleService.

--- src/models/saleProductModel.js
+++ src/models/saleProductModel.js
@@ -1,5 +1,11 @@
 const connection = require('./connection');
 
+const createSaleProduct = async ({ productId, quantity }, saleId) => connection
+  .execute(
+    'INSERT INTO sales_products (product_id, quantity, sale_id) VALUE (?, ?, ?)',
+    [productId, quantity, saleId],
+  );
+
 const readSaleProductBySaleId = async (saleId) => {
   const [result] = await connection.execute(
     'SELECT product_id AS productId, quantity FROM sales_products WHERE sale_id = ?',
@@ -7,12 +13,6 @@ const readSaleProductBySaleId = async (saleId) => {
   );
   return result;
 };
-
-const createSaleProduct = async ({ productId, quantity }, saleId) => connection
-  .execute(
-    'INSERT INTO sales_products (product_id, quantity, sale_id) VALUE (?, ?, ?)',
-    [productId, quantity, saleId],
-  );
 
 const deleteSaleProduct = async (saleId) => connection
   .execute(
--- src/services/saleService.js
+++ src/services/saleService.js
@@ -1,6 +1,21 @@
 const { saleModel, saleProductModel } = require('../models');
 const saleProductService = require('./saleProductService');
 
+const registerSale = async (sales) => {
+  try {
+    const date = new Date();
+    const saleId = await saleModel.createSale(date);
+    await Promise.all(saleProductService.registerSaleProduct(sales, saleId));
+    const sale = {
+      id: saleId,
+      itemsSold: await saleProductService.findSaleProductBySaleId(saleId),
+    };
+    return { type: null, content: sale };
+  } catch (err) {
+    return { type: 'INTERNAL_ERROR', message: 'Internal error' };
+  }
+};
+
 const findSales = async (id) => {
   try {
     const sales = {
@@ -18,26 +33,28 @@ const findSales = async (id) => {
   }
 };
 
-const registerSale = async (sales) => {
+const changeSale = async (id, sales) => {
   try {
-    const date = new Date();
-    const saleId = await saleModel.createSale(date);
-    await Promise.all(saleProductService.registerSaleProduct(sales, saleId));
-    const sale = {
-      id: saleId,
-      itemsSold: await saleProductService.findSaleProductBySaleId(saleId),
-    };
-    return { type: null, content: sale };
+    if (await saleModel.readSaleById(id)) {
+      await saleProductModel.deleteSaleProduct(id);
+      await Promise.all(saleProductService.registerSaleProduct(sales, id));
+      const sale = {
+        saleId: id,
+        itemsUpdated: await saleProductService.findSaleProductBySaleId(id),
+      };
+      return { type: null, content: sale };
+    }
+    return { type: 'SALE_NOT_FOUND', message: 'Sale not found' };
   } catch (err) {
     return { type: 'INTERNAL_ERROR', message: 'Internal error' };
   }
 };
 
-const removeSale = async (saleId) => {
+const removeSale = async (id) => {
   try {
-    if (await saleModel.readSaleById(saleId)) {
-      await saleProductModel.deleteSaleProduct(saleId);
-      await saleModel.deleteSale(saleId);
+    if (await saleModel.readSaleById(id)) {
+      await saleProductModel.deleteSaleProduct(id);
+      await saleModel.deleteSale(id);
       return { type: null };
     }
     return { type: 'SALE_NOT_FOUND', message: 'Sale not found' };
@@ -47,6 +64,7 @@ const removeSale = async (saleId) => {
 };
 
 module.exports = {
+  changeSale,
   findSales,
   registerSale,
   removeSale,
