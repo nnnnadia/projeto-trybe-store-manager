df61b80791f9125329586a5e3df4487a1a96ca08
nnnnadia <tristaonadia@gmail.com>
2022-09-29 12:15:53 -0300

    feat: create `deleteSale` on saleController.

--- src/controllers/saleController.js
+++ src/controllers/saleController.js
@@ -14,7 +14,15 @@ const postSale = async (req, res, next) => {
   next(result);
 };
 
+const deleteSale = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await saleService.removeSale(id);
+  if (!result.type) return res.status(204).json({});
+  next(result);
+};
+
 module.exports = {
+  deleteSale,
   getSales,
   postSale,
 };
